6a171e2e856d78af32f62506e9bde5d993c69365
Naol22 <148576561+Naol22@users.noreply.github.com>
2025-02-07 12:22:56 +0300

    Add files via upload

---
+++ src/components/Content.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Content = () => {
+    return (
+        <div class="mt-20 mb-20">
+            
+
+            
+<h1 class=" text-center mb-4 text-4xl font-extrabold leading-none tracking-tight text-yellow-800 md:text-5xl lg:text-6xl dark:text-yellow-800">We invest in the world’s potential</h1>
+<p class="text-center text-lg  text-gray-500 lg:text-3xl sm:px-16 xl:px-48 dark:text-black font-extralight text-2xl">Ethiopia stands at the forefront of a transformative opportunity to position itself as a global leader in Bitcoin mining.</p>
+<p class=" text-center mb-3 text-gray-500 dark:text-gray-800 p-20 font-extralight text-2xl ">Entrepreneurs and investors worldwide are increasingly drawn to Ethiopia’s abundant hydropower resources, recognising the country as an ideal destination for sustainable Bitcoin mining operations. This burgeoning industry not only highlights Ethiopia’s potential to attract signicant foreign investment but also offers a pathway to boosting the nation’s gross domestic product. By capitalising on this momentum, Ethiopia could redene its economic landscape and establish itself as a cornerstone of the global Bitcoin mining ecosystem.</p>
+
+
+
+        </div>
+    );
+}
+
+export default Content;
